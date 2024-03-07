f3c342628758becd7d4f7b9dcbb9141d0900dae8
Guilherme Wind <fc58640@alunos.fc.ul.pt>
2024-03-07 08:41:01 +0000

    tentei brincar um bocado com o enunciado

--- IoTDevice.java
+++ IoTDevice.java
@@ -1,12 +1,45 @@
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.util.Scanner;
+
 public class IoTDevice{
-    private String serverAddress;
-    private int devId;
-    private String userId;
 
-    public IoTDevice(String serverAddress, int devId, String userId){
-        this.serverAddress = serverAddress; // Falta verificar se contem o port ou não
-        this.devId = devId;
-        this.userId = userId;
+    public static void main(String[] args){
 
+        String[] auxAddress = args[0].split(":");
+        String serverIp = auxAddress[0];
+
+        int devId = Integer.parseInt(args[1]);
+        String userId = args [2];
+
+        int serverPort = 0;
+        
+        if(auxAddress.length == 1){
+            serverPort = 12345;
+        } else {
+            serverPort = Integer.parseInt(auxAddress[1]);
+        }
+
+        Socket clientSocket = new Socket(serverIp, serverPort);
+        try {
+            ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+            ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
+            
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter password");
+            String password = sc.nextLine();
+            String fromServer = (String) in.readObject();
+
+            System.out.println("Mensagem do servidor: " + fromServer);
+
+        } catch (UnknownHostException e) {
+            
+        } catch (IOException e) {
+        
+        } catch (ClassNotFoundException e) {
+
+        }
     }
 }
--- IoTServer.java
+++ IoTServer.java
@@ -1,7 +1,6 @@
 public class IoTServer{
-    private string port;
-
-    public IoTServer(String port){
-    this.port = port === null ? "12345" : port;
+    
+    public static void main(String[] args){
+        
     }
 }
