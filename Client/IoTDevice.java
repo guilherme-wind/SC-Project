c1deb73a37de6b68632e6a9054f682c487df85e8
Xiting Wang <wangxiting01917@gmail.com>
2024-03-08 15:39:18 +0000

    initialize added

--- Client/IoTDevice.java
+++ Client/IoTDevice.java
@@ -1,70 +1,58 @@
 package Client;
+
 import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 
-public class IoTDevice{
+public class IoTDevice {
+
+    private static final int DEFAULT_SERVERPORT = 12345;
+
+    // Simple UI
+    private static IoTCLI cli;
+
+    private static Socket clientSocket;
+    private static ObjectOutputStream outputStream;
+    private static ObjectInputStream inputStream;
+
+    private static int devId;
+    private static String userId;
 
     public static void main(String[] args) throws IOException {
+
+        // Initialize cli
+        cli = IoTCLI.getInstance();
 
         // Command line argument validation
         int args_verify = verifyCmdArgs(args);
         if (args_verify > 0) {
-            System.out.println("Incorrect command line arguments!");
+            cli.printErr("Wrong input arguments!");
+            cli.close();
         }
 
-        // Extract socket
-        String[] socket = args[0].split(":", 2);
-        String serverIp = socket[0];
-        int serverPort = 0;
-        if(socket.length == 1){
-            // Use default port
-            serverPort = 12345;
-        } else {
-            serverPort = Integer.parseInt(socket[1]);
+        // Initialize class fields using command line arguments
+        int init_res = initialize(args);
+        if (init_res > 0) {
+            cli.printErr("Error initializing device!");
+            cli.close();
         }
 
-        // Extract devide id
-        int devId = Integer.parseInt(args[1]);
-
-        // Extract user id
-        String userId = args[2];
 
         
-
-        try {
-            Socket clientSocket = new Socket(serverIp, serverPort);
-            ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
-            ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
-            
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter password");
-            String password = sc.nextLine();
-            String fromServer = (String) in.readObject();
-
-            System.out.println("Mensagem do servidor: " + fromServer);
-
-        } catch (UnknownHostException e) {
-            //sexo anala é bom e eu gosto
-        } catch (IOException e) {
-         //hahahahaa
-        } catch (ClassNotFoundException e) {
-
-        }
     }
 
     /**
      * Verifies if the command line arguments complies with
      * the format <socket> <dev-id> <user-id>.
      * @param args
-     *      Command line arguments
+     *      Command line arguments.
      * @return
      *      0 if all the inputs are correct;
      *      -1 if number of arguments is wrong;
-     *      -2 if socket is different to <ip>:<port> or <ip>, only supporting IPv4;
-     *      -3 if dev-id is not digit-only;
-     *      -4 if user-id doesn't meet the expected format;
+     *      -2 if socket ip doesn't match IPv4 format;
+     *      -3 if socket port isn't a valid value, i.e., [0, 65535].
+     *      -4 if dev-id is not a number;
+     *      -5 if user-id doesn't meet the expected format;
      */
     private static int verifyCmdArgs(String[] args) {
         if (args.length != 3)
@@ -80,9 +68,11 @@ public class IoTDevice{
         // In case the port is provided
         if (socket.length == 2) {
             try {
-                Integer.parseInt(socket[1]);
+                int port = Integer.parseInt(socket[1]);
+                if (port < 0 || port > 65535)
+                    return -3;
             } catch (NumberFormatException e) {
-                return -2;
+                return -3;
             }
         }
         
@@ -90,11 +80,52 @@ public class IoTDevice{
         try {
             Integer.parseInt(args[1]);
         } catch (NumberFormatException e) {
-            return -3;
+            return -4;
         }
 
         // Verify <user-id>
         // TODO not used for now
+        return 0;
+    }
+
+    /**
+     * Initializes the class fields using the command line arguments.
+     * @param args
+     *      Command line arguments.
+     * @return
+     *      0 if initialization finished correctly;
+     *      -1 if host ip is unknown;
+     *      -2 if errors occured while creating a socket or 
+     *         getting input or output streams from socket;
+     */
+    private static int initialize(String[] args) {
+        // Extract socket
+        String[] socket = args[0].split(":", 2);
+        String serverIp = socket[0];
+        int serverPort = 0;
+        if(socket.length == 1)
+            // Use default port
+            serverPort = DEFAULT_SERVERPORT; 
+        else
+            serverPort = Integer.parseInt(socket[1]);
+        
+        // Extract devide id
+        devId = Integer.parseInt(args[1]);
+
+        // Extract user id
+        userId = args[2];
+        
+        // Estabilish connection with server
+        try {
+            clientSocket = new Socket(serverIp, serverPort);
+            outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+            inputStream = new ObjectInputStream(clientSocket.getInputStream());
+        } catch (UnknownHostException e) {
+            return -1;
+        } catch (IOException e) {
+            return -2;
+        }
+
         return 0;
     }
 }
