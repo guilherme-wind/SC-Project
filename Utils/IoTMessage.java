c326b93c4ef9eb1400e12e113ce1c3a8542be850
Xiting Wang <wangxiting01917@gmail.com>
2024-03-08 15:40:22 +0000

    defining message type

--- Utils/IoTMessage.java
+++ Utils/IoTMessage.java
@@ -1,13 +1,22 @@
 package Utils;
 
+import Utils.IoTOpcodes;
+
 /**
  * Class that defines the type of message exchanged between server and client
  */
 public class IoTMessage {
+
+    public IoTOpcodes opcode;
+
+    public String userid;
+
+    public String userpwd;
 
     
     
     public IoTMessage() {
 
     }
+
 }
--- Utils/IoTOpcodes.java
+++ Utils/IoTOpcodes.java
@@ -1,10 +1,21 @@
 package Utils;
 
 public enum IoTOpcodes {
+
+    // ---------------------------
+    //      Authentication
+    // ---------------------------
     // Client request operations
     VALIDATE_DEVICE, 
     VALIDADE_USER, 
-    VALIDATE_PROGRAM
+    VALIDATE_PROGRAM,
 
     // Server responses
+    WRONG_PWD,
+    OK_NEW_USER,
+    OK_USER,
+    NOK_DEVID,
+    OK_DEVID,
+    NOK_TESTED,
+    OK_TESTED
 }
