e534a377a651482801a712e1b1151f1ecc577e72
guilherme-marcello <guilemosm@gmail.com>
2024-03-09 02:32:49 +0000

    Included example of in-mem db

---
+++ src/Server/IoTServerDatabase.java
@@ -0,0 +1,64 @@
+package src.Server;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import src.Server.model.Domain;
+import src.Server.model.User;
+import src.Server.model.Device;
+import src.Utils.IoTOpcodes;
+
+
+public class IoTServerDatabase {
+    private final Map<String, Domain> domains;
+    private final Map<String, User> users;
+    private final Map<String, Device> devices;
+
+    public IoTServerDatabase() {
+        this.domains = new HashMap<>();
+        this.users = new HashMap<>();
+        this.devices = new HashMap<>();
+    }
+
+    public IoTOpcodes createDomain(User as, String domainName) {
+        if (this.domains.containsKey(domainName))
+            return IoTOpcodes.NOK_ALREADY_EXISTS;
+
+        this.domains.put(domainName, new Domain(domainName, as));
+        return IoTOpcodes.OK_ACCEPTED;
+    }
+
+    public IoTOpcodes addUserToDomain(User as, String userName, String domainName) {
+        if (!this.domains.containsKey(domainName))
+            return IoTOpcodes.NOK_NO_DOMAIN;
+
+        if (!this.users.containsKey(userName))
+            return IoTOpcodes.NOK_NO_USER;
+
+        Domain domain = this.domains.get(domainName);
+        if (!domain.ownedBy(as))
+            return IoTOpcodes.NOK_NO_PERMISSIONS;
+        
+        User user = this.users.get(userName);
+        domain.addUser(user);
+        return IoTOpcodes.OK_ACCEPTED;
+    }
+
+    public IoTOpcodes createDevice(String name, String ownerName) {
+        Device device = new Device(name, ownerName);
+        this.devices.put(device.getName(), device);
+        return IoTOpcodes.OK_ACCEPTED;
+    }
+
+    public IoTOpcodes registerDeviceToDomain(User as, Device device, String domainName) {
+        if (!this.domains.containsKey(domainName))
+            return IoTOpcodes.NOK_NO_DOMAIN;
+
+        Domain domain = this.domains.get(domainName);
+        if (!domain.contains(as))
+            return IoTOpcodes.NOK_NO_PERMISSIONS;
+
+        domain.registerDevice(device);
+        return IoTOpcodes.OK_ACCEPTED;
+    }
+}
