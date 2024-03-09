d5b0e881b0b4ba2b81d25cc1ac203defdc87a6fd
guilherme-marcello <guilemosm@gmail.com>
2024-03-09 14:48:22 +0000

    Added logs for IoTServer

--- src/Server/IoTServer.java
+++ src/Server/IoTServer.java
@@ -4,8 +4,10 @@ import java.net.ServerSocket;
 
 public class IoTServer{
     public static void main(String[] args) {
+        System.out.println("Starting server...");
         try (ServerSocket socket = new ServerSocket(12345)) {
             while (true) {
+                System.out.println("Waiting for new connection...");
                 IoTServerThread thread = new IoTServerThread(socket.accept());
                 thread.start();
                 // TODO store running threads in a pool, so we can exit gracefully
