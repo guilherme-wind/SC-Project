72121de1e5ea9e01321a7276dc653f4f9d12d400
guilherme-marcello <guilemosm@gmail.com>
2024-03-09 13:47:13 +0000

    Updated Server to use IoTServerThread

--- src/Server/IoTServer.java
+++ src/Server/IoTServer.java
@@ -1,55 +1,17 @@
-package src.Server;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+package src.server;
+
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
 
 public class IoTServer{
-    private static Map<String, String> usersMap = new HashMap<>();
-
-
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        ServerSocket srvSocket = new ServerSocket(12345);
-        System.out.println("Server started. Waiting for clients...");
-
-        while(true){
-            Socket cliSocket = srvSocket.accept();
-            System.out.println("Client connected: " + cliSocket);
-            Threads thread = new Threads(cliSocket);
-
+    public static void main(String[] args) {
+        try (ServerSocket socket = new ServerSocket(12345)) {
+            while (true) {
+                IoTServerThread thread = new IoTServerThread(socket.accept());
+                thread.start();
+                // TODO store running threads in a pool, so we can exit gracefully
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-    }
-
-     class Threads extends Thread {
-        private Socket cliSocket;
-
-        Threads(Socket cliSocket){
-            this.cliSocket = cliSocket;
-        }
-
-
-        @Override
-        public void run() {
-            try {
-                ObjectOutputStream out = new ObjectOutputStream(cliSocket.getOutputStream());
-                ObjectInputStream in = new ObjectInputStream(cliSocket.getInputStream());
-
-                String username = (String) in.readObject();
-                String password = (String) in.readObject();
-                if(authenticateUser(username, password)) {
-
-                }
-            } catch (IOException | ClassNotFoundException e){
-                System.err.println("Erro");
-            }
-        }
-
-    private static boolean authenticateUser(String username, String password) {
-        return false;
     }
 }
