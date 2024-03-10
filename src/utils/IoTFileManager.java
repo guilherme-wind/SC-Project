9ded4e18fd548ca717fd62bef5e89ab32605c6c5
Xiting Wang <wangxiting01917@gmail.com>
2024-03-10 17:56:29 +0000

    loadUsersFromText implemented

--- src/utils/IoTFileManager.java
+++ src/utils/IoTFileManager.java
@@ -1,16 +1,23 @@
 package src.utils;
 
+import java.io.File;
 import java.util.Map;
-
+import java.util.Scanner;
 
 import src.server.model.User;
 
 public class IoTFileManager {
+
+    // Not used for now
+    private Scanner sc;
+
     public IoTFileManager() {
     }
 
     /**
-     * Reads content from plain text file and loads users
+     * Reads content from plain text file and loads users.
+     * Text file must have format
+     * username:password
      * @param filePath
      * @param users
      *      Map to where data will be loaded.
@@ -18,8 +25,34 @@ public class IoTFileManager {
      *      0 if loaded successfully;
      *      -1 if file doesn't exist;
      *      -2 if no permissions to access file;
+     *      -3 if arguments are invalid;
      */
     public int loadUsersFromText(String filePath, Map<String, User> users) {
+        if (filePath == null || users == null)
+            return -3;
+
+        File file = new File(filePath);
+        if (!file.exists() || file.isDirectory())
+            return -1;
+        if (!file.canRead())
+            return -2;
+
+        try {
+            Scanner fileScanner = new Scanner(file);
+            while (fileScanner.hasNextLine()) {
+                String[] tokens = fileScanner.nextLine().split(":", 2);
+                if (tokens.length < 2)
+                    continue;
+                
+                User user = new User(tokens[0], tokens[1]);
+                users.put(tokens[0], user);
+            }
+
+            fileScanner.close();
+        } catch (Exception e) {
+            return -1;
+        }
+        
         return 0;
     }
 
