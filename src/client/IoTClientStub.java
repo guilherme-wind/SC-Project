c4e71392c9ab14165ef46bea1a6dc235e424dc2b
guilherme-marcello <guilemosm@gmail.com>
2024-03-11 21:00:12 +0000

    Changed from float to byte (temperatures)

--- src/client/IoTClientStub.java
+++ src/client/IoTClientStub.java
@@ -340,7 +340,7 @@ public class IoTClientStub {
      *          - Domain doesn't exist;
      *          - Socket error;
      */
-    protected float[] getTemp(String domainName) {
+    protected byte[] getTemperaturesInDomain(String domainName) {
         IoTMessageType request = new IoTMessage();
         request.setOpCode(IoTOpcodes.GET_TEMP);
         request.setDomainName(domainName);
@@ -353,10 +353,12 @@ public class IoTClientStub {
             return null;
 
         IoTOpcodes respcode = response.getOpcode();
-        float[] temps = response.getTemps();
-        if (respcode.equals(IoTOpcodes.OK_ACCEPTED) &&
-            temps != null)
-            return temps;
+        byte[] data = response.getData();
+
+        System.out.println(new String(data));
+        if (respcode.equals(IoTOpcodes.OK_ACCEPTED) && data != null)
+            return data;
+
         return null;
     }
 
