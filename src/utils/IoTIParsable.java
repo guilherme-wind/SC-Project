374bf14163ca73071cc8198f3c9de674a5af746a
Xiting Wang <wangxiting01917@gmail.com>
2024-03-11 19:03:30 +0000

    interface to parse objects to strings

---
+++ src/utils/IoTIParsable.java
@@ -0,0 +1,13 @@
+package src.utils;
+
+public interface IoTIParsable {
+    
+    /**
+     * Gets a string representation of the object.
+     * @return
+     *      String representation of the object.
+     */
+    public String parseToSerial();
+
+    
+}
