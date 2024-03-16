111bea4ee448084c6767107aebb3ec822483cbea
guilherme-marcello <guilemosm@gmail.com>
2024-03-16 12:37:01 +0000

    Added parseFrom serial to interface

--- src/utils/IoTIParsable.java
+++ src/utils/IoTIParsable.java
@@ -9,5 +9,17 @@ public interface IoTIParsable {
      */
     public String parseToSerial();
 
+    /**
+     * Parses a serial representation into an object.
+     * @param serial
+     *      The serial representation to parse.
+     * @return
+     *      An instance of the implementing class.
+     */
+    public static IoTIParsable parseFromSerial(String serial) {
+        // Implementation specific to each class implementing IoTIParsable
+        return null;
+    }
+
     
 }
