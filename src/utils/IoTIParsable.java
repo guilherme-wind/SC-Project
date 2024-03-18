118a1672aac33655e75760f423900efa481f4987
Xiting Wang <wangxiting01917@gmail.com>
2024-03-18 18:44:23 +0000

    update javadoz

--- src/utils/IoTIParsable.java
+++ src/utils/IoTIParsable.java
@@ -4,18 +4,28 @@ public interface IoTIParsable {
     
     /**
      * Gets a string representation of the object.
+     * @deprecated
+     *      To serialize object in order to write to 
+     *      persistent storage, please use 
+     *      {@link IoTFileManager#writeObjectToFile(String, Object)}
      * @return
      *      String representation of the object.
      */
+    @Deprecated
     public String parseToSerial();
 
     /**
      * Parses a serial representation into an object.
+     * @deprecated
+     *      To serialize object in order to write to 
+     *      persistent storage, please use 
+     *      {@link IoTFileManager#readObjectFromFile(String)}
      * @param serial
      *      The serial representation to parse.
      * @return
      *      An instance of the implementing class.
      */
+    @Deprecated
     public static IoTIParsable parseFromSerial(String serial) {
         // Implementation specific to each class implementing IoTIParsable
         return null;
