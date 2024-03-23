b6200c62a7bf20c7b1d1978f80b77cb00a0adbd0
guilherme-marcello <guilemosm@gmail.com>
2024-03-23 13:50:06 +0000

    Added arg validation for server (custom port)

--- src/server/IoTServer.java
+++ src/server/IoTServer.java
@@ -4,21 +4,32 @@ import java.net.ServerSocket;
 import java.util.LinkedList;
 import java.util.List;
 
+import utils.IoTCLI;
+
 public class IoTServer{
+    private static final String USAGE = "USAGE: IoTServer <port> (optional)";
+
+    private static IoTCLI cli;
 
     // Store running threads
     private static List<IoTServerThread> threads;
+    private static Thread main;
     private static ServerSocket socket;
     public static void main(String[] args) {
-        System.out.println("Starting server...");
-        threads = new LinkedList<IoTServerThread>();
-        Thread main = Thread.currentThread();
+        // Command line argument validation
+        if (verifyCmdArgs(args) < 0) {
+            cli.printErr("Wrong input arguments!");
+            cli.print(USAGE);
+            return;
+        }
 
-        // Associates shutdown signal with it's handler
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> { shutdown(main); }));
+        // Initialize class fields using command line arguments
+        if (initialize(args) < 0) {
+            cli.printErr("Failed initializing server!");
+            return;
+        }
 
         try {
-            socket = new ServerSocket(12345);
             while (true) {
                 System.out.println("Waiting for new connection...");
                 IoTServerThread thread = new IoTServerThread(socket.accept());
@@ -27,25 +38,83 @@ public class IoTServer{
                 thread.start();
             }
         } catch (Exception e) {
-            System.out.println("Fui interrumpido!!!");
-            try {
-                socket.close();
-            } catch (Exception e1){
-
-            }
+            cli.print("Failed to accept incoming connection. Shutting server down...");
             e.printStackTrace();
+            shutdown();
             System.exit(0);
         }
+    }
+
+
+    /**
+     * Verifies if the command line arguments complies with
+     * the format <port> (optional).
+     * @param args
+     *      Command line arguments.
+     * @return
+     *      0 if all the inputs are correct;
+     *      -1 if listening port isn't a valid value, i.e., [0, 65535].
+     */
+    private static int verifyCmdArgs(String[] args) {
+        if (args.length <= 0)
+            return 0;
+        
+        try {
+            int port = Integer.parseInt(args[0]);
+            if (port < 0 || port > 65535)
+                return -1;
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+
+        return 0;
+    }
+
+    /**
+     * Initializes the class fields using the command line arguments.
+     * @param args
+     *      Command line arguments.
+     * @return
+     *      0 if initialization finished correctly;
+     *      -1 if error while initializing;
+     */
+    private static int initialize(String[] args) {
+        try {
+            // Initialize cli
+            cli = IoTCLI.getInstance();
+            cli.print("Starting server...");
+
+            // Associates shutdown signal with it's handler
+            main = Thread.currentThread();
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> { shutdown(); }));
+            threads = new LinkedList<IoTServerThread>();
+
+            // Creates server socket
+            socket = new ServerSocket(
+                Integer.parseInt(args[0])
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+        return 0;
     }
 
     /**
      * Closes all threads and exit.
      */
-    private static void shutdown(Thread main) {
+    private static void shutdown() {
         for (IoTServerThread ioTServerThread : threads) {
             ioTServerThread.interrupt();
         }
+
+        try {
+            socket.close();
+        } catch (Exception e1){
+
+        }
         main.interrupt();
+        
         System.out.println("Went to mewing, byebye!");
     }
 }
